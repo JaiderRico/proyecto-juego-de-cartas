00216b356d8a70bc466b5f028bc70e245de39643
Jaider Gomez <jaider.grico@gmail.com>
2025-03-28 14:36:23 -0500

    JUEGO TERMINADO

--- Juego.cpp
+++ Juego.cpp
@@ -18,11 +18,11 @@ Juego::Juego(vector<string> nombres) {
 void Juego::generarMazo() {
     vector<string> colores = {"Rojo", "Azul", "Verde", "Amarillo"};
     srand(time(0));
-    for(int i = 0; i < 8; i++ ){
+    for(int i = 0; i < 9; i++ ){
        for(int j = 0; j < 4; j++){
         mazo.push_back(Carta(i,colores[j]));
-        }; 
-    };
+        } 
+    }
     random_shuffle(mazo.begin(), mazo.end());
 }
 
@@ -33,65 +33,76 @@ void Juego::repartirCartas() {
         jugadores[i % numJugadores].agregarCarta(mazo[i]);
     }
 }
+
+
 void Juego::jugar() {
-    Jugador* jugadorActual = &jugadores[0];
-    
-    while (!jugadores[0].mano.empty()) {
+    int turno = 0; 
+
+    while (!jugadores[0].mano.empty() && !jugadores[1].mano.empty()) {
         cout << "===== Nueva Ronda =====" << endl;
-        
+
+        Jugador* jugadorActual = &jugadores[turno]; 
+        Jugador* segundoJugador = &jugadores[1 - turno]; 
+
         cout << "\n" << jugadorActual->nombre << ", es tu turno." << endl;
         cout << "Tu mano:" << endl;
-        for (int j = 0; j < jugadorActual->mano.size(); j++) {
+        for (size_t j = 0; j < jugadorActual->mano.size(); j++) {
             cout << j << ": " << jugadorActual->mano[j].numero << " " 
                  << jugadorActual->mano[j].color << endl;
         }
-        
+
         int id;
         cout << "Elige el índice de la carta a jugar: ";
         cin >> id;
-        
-        Carta cartaInicial = jugadorActual->jugarCarta(id); 
-        if (jugadorActual != &jugadores[0]){
-            segundoJugador = jugadores[0];
-        }else{
-            segundoJugador = jugadores[1];
+        while (id < 0 || id >= (int)jugadorActual->mano.size()) {
+            cout << "Índice inválido. Intenta de nuevo: ";
+            cin >> id;
         }
+
+        Carta cartaInicial = jugadorActual->jugarCarta(id);
+
         cout << "\n" << segundoJugador->nombre << ", es tu turno." << endl;
         cout << "Tu mano:" << endl;
-        for (int j = 0; j < segundoJugador->mano.size(); j++) {
+        for (size_t j = 0; j < segundoJugador->mano.size(); j++) {
             cout << j << ": " << segundoJugador->mano[j].numero << " " 
                  << segundoJugador->mano[j].color << endl;
         }
-        
+
         cout << "Elige el índice de la carta a jugar: ";
         cin >> id;
-        
-        Carta cartaSegunda = segundoJugador->jugarCarta(id);
-        
-        if (cartaInicial.color != cartaSegunda.color){
-            jugadorActual.acumulador += 2;
-        }else { if(cartaInicial.numero> cartaSegunda.numero){
-                    jugadorActual.acumulador += 2;
-                }else{ if(cartaInicial.numero < cartaSegunda.numero){
-                    segundoJugador.acumulador += 2;
-                    }else{
-                        segundoJugador.acumulador += 2;
-                    }
-                }
-
+        while (id < 0 || id >= (int)segundoJugador->mano.size()) {
+            cout << "Índice inválido. Intenta de nuevo: ";
+            cin >> id;
         }
+
+        Carta cartaSegunda = segundoJugador->jugarCarta(id);
+
+        if (cartaInicial.color != cartaSegunda.color) {
+            jugadorActual->acumulador += 2;
+        } else { 
+            if (cartaInicial.numero > cartaSegunda.numero) {
+                jugadorActual->acumulador += 2;
+            } else {
+                segundoJugador->acumulador += 2;
+            }
+        }
+
+        turno = 1 - turno;
     }
 
     determinarGanador();
 }
 
 void Juego::determinarGanador() {
-        if (jugadores[i].acumulador > jugadores[i+1].acumulador){
-            cout << "===== Ganador final: " << jugadores[i].nombre << " con " 
-            << jugadores[i].acumulador << " cartas acumuladas! =====" << endl;
-        }else{
-            cout << "===== Ganador final: " << jugadores[i+1].nombre << " con " 
-            << jugadores[i+1].acumulador << " cartas acumuladas! =====" << endl;
+    Jugador* ganador = &jugadores[0];
+    
+    for (size_t i = 1; i < jugadores.size(); i++) {
+        if (jugadores[i].acumulador > ganador->acumulador) {
+            ganador = &jugadores[i];
         }
+    }
 
+    cout << "===== Ganador final: " << ganador->nombre << " con " 
+         << ganador->acumulador << " puntos! =====" << endl;
 }
+
--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# proyecto-juego-de-cartas
+# Juego de Cartas - C++  
+
+##  Descripción  
+Este es un juego de cartas basado en C++ en el que dos jugadores compiten entre sí. El objetivo es jugar cartas con estrategia y acumular puntos para ganar la partida.  
+
+##  Reglas del Juego  
+1. Se reparten cartas de diferentes colores y números entre los jugadores.  
+2. Cada jugador, en su turno, debe seleccionar una carta de su mano.  
+3. El jugador con la carta más alta o del mismo color de la carta inicial gana la ronda  
+
+##  Tecnologías Utilizadas  
+- Lenguaje: **C++**  
+- Librerías: `<iostream>`, `<vector>`, `<algorithm>`, `<cstdlib>`, `<ctime>`  
+
 ## UML
-![UML](/imagenes/Diagrama%20en%20blanco.png)
+(imagenes/UML.png)
